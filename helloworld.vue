1147c72c0b1ab86f52382933a2f5083fa33ce555
fuyuanhao <1633011180@qq.com>
2021-03-06 22:00:37 +0800

    hello,world!

---
+++ helloworld.vue
@@ -0,0 +1,18 @@
+<template>
+	<div id="app">
+	  {{ message }}
+	</div>
+</template>
+
+<script>
+	new Vue({
+	  el: '#app',
+	  data: {
+		message: 'Hello Vue.js!'
+	  }
+	})
+</script>
+
+<style>
+
+</style>
